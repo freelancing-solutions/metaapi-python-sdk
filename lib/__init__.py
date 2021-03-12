91f675360203c9f3940075f5f2f6bc7a3f989719
Kostiantyn B <kostiantyn.b.as@agiliumlabs.com>
2021-03-12 18:10:39 +0000

    #PS-386 - remove copyfactory sdk

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,4 +2,4 @@ from .metaApi.metaApi import MetaApi
 from .metaApi.historyStorage import HistoryStorage
 from .metaApi.memoryHistoryStorage import MemoryHistoryStorage
 from .clients.metaApi.synchronizationListener import SynchronizationListener
-from .copyFactory.copyFactory import CopyFactory
+from metaapi_cloud_copyfactory_sdk import CopyFactory
